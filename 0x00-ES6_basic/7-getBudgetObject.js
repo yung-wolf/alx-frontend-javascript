013083471ad724771c281f14bcd181817373f583
Eli Bansah (yung-wolf) <edmundbansah@gmail.com>
2024-04-22 20:53:33 +0000

    Object property value shorthand syntax

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,11 @@
+// Object property value shorthand syntax
+
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+}
