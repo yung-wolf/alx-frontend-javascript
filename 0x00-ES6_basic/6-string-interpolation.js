faebf73a2829302b6f5ff89b084944d4d1abd9a8
Eli Bansah (yung-wolf) <edmundbansah@gmail.com>
2024-04-22 20:57:49 +0000

    fixed typo

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -9,5 +9,5 @@ export default function getSanFranciscoDescriptioin() {
     capita: '$178,479',
   };
 
-  return `As of ${year}, it was the seventh-hightest income county in the United States, with a per capita personal income of ${budget.income}. As of 2015, San Francisco proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
+  return `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income of ${budget.income}. As of 2015, San Francisco proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
 }
