02ef5797ab2b97584412401ea1c544c1819c05a4
Eli Bansah (yung-wolf) <edmundbansah@gmail.com>
2024-04-23 01:29:19 +0000

    Create a report object

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,13 @@
+// Write a function named createReportObject whose parameter, employeesList,
+// is the return value of the previous function createEmployeesObject.
+
+export default function createReportObject(employeesList) {
+  const obj = {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+
+  return obj;
+}
