577c396aaa4a1f1f2f0eb3944c57ab49f9be9bd0
Eli Bansah (yung-wolf) <edmundbansah@gmail.com>
2024-04-23 20:01:18 +0000

    Change body value to uppercase

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,7 +1,7 @@
 export default function getFullResponseFromAPI(success) {
   const myPromise = new Promise((resolve, reject) => {
     if (success === true) {
-      resolve({ status: 200, body: 'success' });
+      resolve({ status: 200, body: 'Success' });
     } else {
       reject(new Error('The fake API is not working currently'));
     }
