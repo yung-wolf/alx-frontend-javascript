be21f0ed652818d9aa48b66df281465dabecf7d6
Eli Bansah (yung-wolf) <edmundbansah@gmail.com>
2024-04-23 21:07:10 +0000

    modified:   0x01-ES6_promise/3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const upPhoto = uploadPhoto();
-  const newUser = createUser();
-
-  return Promise.all([newUser, upPhoto])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
-      console.log(values[1].body, values[0].firstName, values[0].lastName);
+      console.log(values[0].body, values[1].firstName, values[1].lastName);
     })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+    .catch(() => console.log('Signup system offline'));
 }
+
+handleProfileSignup();
