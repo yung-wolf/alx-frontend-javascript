f5e6968a2e4f35c3d56c52602b9ff8f02f3c5724
Eli Bansah (yung-wolf) <edmundbansah@gmail.com>
2024-04-24 09:43:15 +0000

    ESlint style fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,7 +6,7 @@ import uploadPhoto from './5-photo-reject';
  * @param {string} firstName - First name of the user
  * @param {string} lastName - Last name of the user
  * @param {string} fileName - Name of the file to be uploaded
- * @returns {Promise} - A promise that resolves with an array containing the profile details or rejects with an error
+ * @returns {Promise} - Promise resolves to an array with the profile info / rejects with an error
  */
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
@@ -14,15 +14,15 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
 
   return Promise.allSettled([userPromise, photoPromise])
     .then((results) => {
-      const settledPromises = results.filter(result => result.status !== 'pending');
-      const pendingPromises = results.filter(result => result.status === 'pending');
+      const settledPromises = results.filter((result) => result.status !== 'pending');
+      const pendingPromises = results.filter((result) => result.status === 'pending');
 
       if (pendingPromises.length === 0 && settledPromises.length === 2) {
         const userProfile = settledPromises[0].value;
         const photoDetails = settledPromises[1].reason;
 
         return [userProfile, photoDetails];
-      } else if (pendingPromises.length > 0) {
+      } if (pendingPromises.length > 0) {
         throw new Error('All promises are not settled');
       } else {
         throw new Error('User signup and photo upload failed');
