66de1c44e0acf5b34ebec623204f81b589f0b1fc
Eli Bansah (yung-wolf) <edmundbansah@gmail.com>
2024-04-24 20:43:39 +0000

    Implement guardrail function

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,23 @@
+/**
+ *
+ * @param {function} mathFunction - a math function
+ */
+
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  // When the mathFunction function is executed
+  // the value returned by the function should be appended to the queue.
+  // If this function throws an error, the error message should be appended to the queue
+  // In every case, the message Guardrail was processed should be added to the queue.
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(error);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
+}
