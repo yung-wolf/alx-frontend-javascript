0f42dbf8d95be0d39df4bdbd020f18ce6f6bf666
Eli Bansah (yung-wolf) <edmundbansah@gmail.com>
2024-04-24 09:39:35 +0000

    Handle multiple promises II

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,9 +1,31 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const user = await signUpUser(firstName, lastName);
-  const photo = await uploadPhoto(fileName);
+/**
+ * handleProfileSignup - Handles the profile signup process
+ * @param {string} firstName - First name of the user
+ * @param {string} lastName - Last name of the user
+ * @param {string} fileName - Name of the file to be uploaded
+ * @returns {Promise} - A promise that resolves with an array containing the profile details or rejects with an error
+ */
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([user, photo]);
+  return Promise.allSettled([userPromise, photoPromise])
+    .then((results) => {
+      const settledPromises = results.filter(result => result.status !== 'pending');
+      const pendingPromises = results.filter(result => result.status === 'pending');
+
+      if (pendingPromises.length === 0 && settledPromises.length === 2) {
+        const userProfile = settledPromises[0].value;
+        const photoDetails = settledPromises[1].reason;
+
+        return [userProfile, photoDetails];
+      } else if (pendingPromises.length > 0) {
+        throw new Error('All promises are not settled');
+      } else {
+        throw new Error('User signup and photo upload failed');
+      }
+    });
 }
