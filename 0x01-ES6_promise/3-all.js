4be064d4836dc71a189b9f1e99a1b17d35694347
Eli Bansah (yung-wolf) <edmundbansah@gmail.com>
2024-04-24 21:12:42 +0000

    Make function async

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
+export default async function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
       console.log(values[0].body, values[1].firstName, values[1].lastName);
