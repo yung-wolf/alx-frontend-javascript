9247b88fea9c31f50c3c9435e80050a5591a41d2
Eli Bansah (yung-wolf) <edmundbansah@gmail.com>
2024-04-25 20:56:13 +0000

    Fix if statement in 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -7,7 +7,7 @@ export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
 
-    if (new.target !== Building && typeof evacuationWarningMessage !== 'function') {
+    if (new.target !== Building && typeof this.evacuationWarningMessage !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
