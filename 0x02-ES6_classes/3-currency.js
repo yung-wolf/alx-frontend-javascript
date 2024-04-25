8bf76556ee6b1f8b781c192067e129868c8ca8af
Eli Bansah (yung-wolf) <edmundbansah@gmail.com>
2024-04-25 16:43:21 +0000

    Implement a class named Currency

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,31 @@
+/**
+ * Currency
+ * @param {string} code
+ * @param {string} name
+ */
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(line) {
+    this._code = line;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
