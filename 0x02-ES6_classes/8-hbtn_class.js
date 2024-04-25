42d1728607fcfd00398f3a1aa3f48bb98f92f555
Eli Bansah (yung-wolf) <edmundbansah@gmail.com>
2024-04-25 21:29:23 +0000

    Implement a class named HolbertonClass:

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,21 @@
+/**
+ * HolbertonClass
+ * @param {Number} size
+ * @param {String} location
+ */
+
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    } if (hint === 'string') {
+      return this._location;
+    }
+    return this._size;
+  }
+}
