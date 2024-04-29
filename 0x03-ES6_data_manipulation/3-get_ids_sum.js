0fbf83234f758111a49d62141687e301bc6a24f7
Eli Bansah (yung-wolf) <edmundbansah@gmail.com>
2024-04-29 20:55:06 +0000

    Definitions for function

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,9 @@
 import getListStudentIds from './1-get_list_student_ids';
-
+/**
+ * getStudentIdsSum
+ * @param {*} studentList 
+ * @returns returns the sum of all the student ids.
+ */
 export default function getStudentIdsSum(studentList) {
   const ids = getListStudentIds(studentList);
   const result = ids.reduce((accumulator, currentValue) => accumulator + currentValue);
